adc57e4a948141752ac1d208545ff1c02a83828a
elihulse5 <elihulse5@gmail.com>
2013-08-05 19:19:13 -0400

    Allowed more than one chance to try a new input

--- HopfieldNetwork.m
+++ HopfieldNetwork.m
@@ -1,7 +1,5 @@
-%copyright 2013 Eli Hulse
-%duplication is allowed as long as my name is referenced somewhere in
-%the application or source code.
-%thank you, enjoy, if you have any questions, email me at 
+%Created: 2013 Eli Hulse
+%thank you for downloading, enjoy, if you have any questions, email me at 
 %elihulse5@gmail.com
 %%
 %trainingData is the initial inputs
@@ -26,20 +24,22 @@ for i=1:x
         weights(i,j)=weight_ij;
     end
 end
-
+weights
 %recalling
-matrix=input('input matrix:');
-fprintf('input:');
-fprintf('%d ',matrix);
-output=rand(size(matrix));
-for i=1:100
-    if ismember(output,trainingDataUnModified,'rows')==1
-       break;
-    else
-        output_absolute=matrix*weights';
-        output=output_absolute>=1;
-        fprintf(' output%d:',i);
-        fprintf('%d ',output);
+for j=1:10
+    matrix=input('input matrix:');
+    fprintf('input:');
+    fprintf('%d ',matrix);
+    output=rand(size(matrix));
+    for i=1:100
+        if ismember(output,trainingDataUnModified,'rows')==1
+           break;
+        else
+            output_absolute=matrix*weights';
+            output=output_absolute>=1;
+            fprintf(' output%d:',i);
+            fprintf('%d ',output);
+        end
+        fprintf('\n');
     end
-    fprintf('\n');
 end
